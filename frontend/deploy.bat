641088136c080021ff01b654f84a9e38246a5f41
mauboru <josue21henrique@gmail.com>
2025-12-09 18:45:37 -0300

    feat: modificacaoes

--- frontend/deploy.bat
+++ frontend/deploy.bat
@@ -1,4 +1,4 @@
 @echo off
 
-ssh root@212.85.19.3 "cd /home/formind-projecthub/htdocs/projecthub.formind.tech && git pull && npm install --force && npm run build"
+ssh root@212.85.19.3 "cd /home/formind-projecthub/htdocs/projecthub.formind.tech/frontend && git pull && npm install --force && npm run build"
 pause
